33e532f5f72a3d8bd94364b92f9888685a5b5077
juan25 <elbartoy@gmail.com>
2024-08-15 23:26:35 -0600

    Ya pude corregir lo de phyton vas a tener que instalar algunas dependencías

--- training.py
+++ training.py
@@ -6,25 +6,17 @@ import numpy as np
 import nltk
 from nltk.stem import WordNetLemmatizer
 
-# Importación de las bibliotecas necesarias
-
-# keras es una biblioteca para construir y entrenar modelos de redes neuronales
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import sgd_experimental
+from keras.optimizers import SGD
 
-# Inicialización del lematizador de WordNet.
-# El lematizador convierte palabras a su forma base o lema.
+# Inicialización del lematizador
 lemmatizer = WordNetLemmatizer()
 
-# Carga del archivo "intents.json" que contiene las intenciones o categorías para el modelo de chatbot.
-# Este archivo generalmente incluye patrones y respuestas para diferentes intenciones.
+# Carga del archivo intents.json
 intents = json.loads(open("intents.json").read())
 
-# Descarga de los paquetes necesarios de NLTK (Natural Language Toolkit):
-# 'punkt': Tokenizador para dividir el texto en palabras o frases.
-# 'wordnet': Base de datos de palabras en inglés, utilizada para el lematizador.
-# 'omw-1.4': Paquete de WordNet Open Multilingual Wordnet.
+# Descarga de los paquetes necesarios de NLTK (Natural Language Toolkit)
 nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
@@ -46,6 +38,7 @@ for intent in intents['intents']:
 # Lematiza las palabras y elimina los caracteres ignorados
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
+classes = sorted(set(classes))
 
 # Guarda las palabras y clases en archivos pickle
 pickle.dump(words, open("words.pkl", "wb"))
@@ -61,13 +54,17 @@ for document in documents:
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
-    oupout_row = list(output_empty)
-    oupout_row[classes.index(document[1])] = 1
-    training.append([bag,oupout_row])
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)
 
-print(training)
-    
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
+
+print(train_x)
+
 
