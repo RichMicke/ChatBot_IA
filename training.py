98a6dfeca83c6fa70d2ba05acf5214ff99c115f4
Eljuan25 <147083751+Eljuan25@users.noreply.github.com>
2024-08-20 21:01:44 -0600

    ChatEstiloweb (#3)
    * Esto es las librerías que ocupariamos para crear el archivo pero tengo problemas con phyton
    
    * Ya pude corregir lo de phyton vas a tener que instalar algunas dependencías
    
    * Esto ya logra que te responda pero tienen poco conocimiento así queséria llenarlo de conocimiento para que pueda crecer
    
    * Estos son los cambios y le agregado más inofrmación a su capacidad para aprender.
    
    * Este ya es el estilo men pero para eso tienes que descargar otra dependencía para que puedas correrla ahorita te pongo todo el el comit

--- training.py
+++ training.py
@@ -11,8 +11,8 @@ import random
 # Inicialización del lematizador
 lemmatizer = WordNetLemmatizer()
 
-# Carga del archivo intents.json
-with open("intents.json") as file:
+# Carga del archivo intents.json (mantén la versión correcta)
+with open("intents.json", encoding='utf-8') as file:
     intents = json.load(file)
 
 # Descarga de los paquetes necesarios de NLTK (Natural Language Toolkit)
@@ -63,8 +63,8 @@ for document in documents:
 random.shuffle(training)
 training = np.array(training, dtype=object)
 
-train_x = np.array([item[0] for item in training])
-train_y = np.array([item[1] for item in training])
+train_x = np.array([item[0] for item in training], dtype=np.float32)
+train_y = np.array([item[1] for item in training], dtype=np.float32)
 
 # Verificar la consistencia de los datos antes de convertir a un array de NumPy
 print(f"Longitudes de bag: {[len(item[0]) for item in training]}")
